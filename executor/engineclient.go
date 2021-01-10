0883f26ed1874c9c506dd1b7ef8cfe092b0be599
zhang lian zhuang <zlianzhuang@163.com>
2021-01-11 02:37:21 +0800

    change engine host for docker-compose test
    change engine from 127.0.0.1 to flinkjobmanager

--- executor/engineclient.go
+++ executor/engineclient.go
@@ -6,7 +6,8 @@ import (
 
 func GetEngine(options string) (engineType string, engineHost string, enginePort string, engineOption string, err error) {
 	engineType = "Flink"
-	engineHost = "127.0.0.1"
+	//engineHost = "127.0.0.1"
+	engineHost = "flinkjobmanager"
 	enginePort = "8081"
 	engineOption = ""
 	fmt.Println("get engine")
